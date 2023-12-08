53324b10dd681373f3abe660e4a87966c5c9d21e
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2023-12-09 00:39:12 +0900

    test bash changed w license

--- test.bash
+++ test.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Tomohiro Hayashi s19c1094hn@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
-
-[ "${out}" = 14 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+[ "$res" = 0 ] && echo OK
+exit $res
