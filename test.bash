85042a3e8c90253dec25452555e02f33cc2ec3ba
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2023-12-27 16:47:34 +0900

    test update 3

--- test.bash
+++ test.bash
@@ -9,16 +9,8 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+out=$(./plus_cal.py <testdata)
+[ "${out}" = 3 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
