ff4f34034fbb5b61513519c6b89b966deb15e50e
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2024-01-23 17:34:01 +0900

    test update 1023/2

--- test.bash
+++ test.bash
@@ -2,18 +2,5 @@
 # SPDX-FileCopyrightText: 2023 Tomohiro Hayashi s19c1094hn@s.chibakoudai.jp
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-      echo NG at Line $1
-      res=1
-}
 
-res=0
-
-### I/O test ###
-out=$(
-    ./plus_cal.py < testdata
-    )
-[ "${out}" = Enter the first number: Enter the second number: Choose operator(+,-,*,/): 3 ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
-exit $res
+./plus_cal < testdata
