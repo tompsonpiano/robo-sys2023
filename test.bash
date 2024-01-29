6154532ee78c2ca62eae8b34ba8c16c389c42f80
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2024-01-29 23:05:44 +0900

    test update 0129/1

--- test.bash
+++ test.bash
@@ -2,4 +2,12 @@
 # SPDX-FileCopyrightText: 2023 Tomohiro Hayashi s19c1094hn@s.chibakoudai.jp
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line ${1}
+	ret=1
+}
+
+ret=0
+
+
 ./plus_cal < testdata
