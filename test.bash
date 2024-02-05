76070eb1145b009820143205b58272a17ebd9573
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2024-02-05 11:25:22 +0900

    test update

--- test.bash
+++ test.bash
@@ -12,17 +12,20 @@ res=0
 
 out=$(./plus_cal < test/testdata)
 
-[ "${out}" = "Enter first number:Enter second number:Select operator(+, -, *, /):5" ]||ng ${LINENO}
+[ "${out}" = "First_number:Second_number:Operator:5" ]||ng ${LINENO}
 
 ###strange input###
+###first number error###
 out=$(./plus_cal < test/testdata2)
 [ "$?" = 1 ]					|| ng ${LINENO}
 [ "${out}" = "First_number:" ]	|| ng ${LINENO}
 
+###second number error###
 out=$(./plus_cal < test/testdata3)
 [ "$?" = 1 ]								|| ng ${LINENO}
 [ "${out}" = "First_number:Second_number:" ]	|| ng ${LINENO}
 
+###operator error###
 out=$(./plus_cal < test/testdata4)
 [ "$?" = 0 ]					|| ng ${LINENO}
 [ "${out}" = "First_number:Second_number:Operator:Please_try_again" ]	|| ng ${LINENO}
